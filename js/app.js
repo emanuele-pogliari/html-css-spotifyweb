96706d150b981d409020b8c2414044a9604acba0
emanuele-pogliari <manu.pogliari@gmail.com>
2024-01-15 14:26:59 +0100

    fix overlay album

---
+++ js/app.js
@@ -0,0 +1,5 @@
+var prova = document.querySelector(".test"); //sarebbe il tasto play
+
+prova.onclick = function () {
+    document.querySelector(".play-progress").style.width = "100%"; //la barra di progressione
+}
